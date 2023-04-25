e8b1fe428d97ab66115f4f145cd8ba0842229f58
Seven <graf.lancelot7@gmail.com>
2023-04-25 22:12:37 +0300

    add first part homework

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import "../css/common.css";
+import "../css/01-gallery.css";
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from "./gallery-items.js";
+
+const refs = {
+  galleryContainer: document.querySelector(".gallery"),
+};
+
+const listItemsMakup = createListItemsMarkup(galleryItems);
+populateMarkup(listItemsMakup);
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+function createListItemsMarkup(elements) {
+  return elements
+    .map((element) => {
+      return `<li class="gallery__item">
+                    <a href="${element.original}" class="gallery__link">
+                        <img class="gallery__image" src="${element.preview}" alt="${element.description}">
+                    </a>
+                    </li>`;
+    })
+    .join("");
+}
+
+function populateMarkup(markup) {
+  refs.galleryContainer.innerHTML = markup;
+}
