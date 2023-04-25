49de8aa53ef2013e057811cc4af3c6ae58246cc6
Seven <graf.lancelot7@gmail.com>
2023-04-26 00:22:36 +0300

    update with second part homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Vimeo from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const refs = {
+  framePlayer: document.querySelector("#vimeo-player"),
+};
+const player = new Vimeo(refs.framePlayer);
+updatingCurrentTime();
+
+player.on("timeupdate", throttle(saveVideoTime, 1000));
+
+function updatingCurrentTime() {
+  const savedTime = localStorage.getItem("videoplayer-current-time");
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+}
+
+function saveVideoTime(data) {
+  localStorage.setItem("videoplayer-current-time", `${data.seconds}`);
+}
